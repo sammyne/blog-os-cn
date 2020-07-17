33d49d47551e41e72f17e1a77a3d5ff744ea7a75
sammyne <lixiangmin01@baidu.com>
2020-07-17 19:35:00 +0800

    feature(08-paging-introduction): demo 'Accessing the Page Tables'

--- 08-paging-introduction/src/main.rs
+++ 08-paging-introduction/src/main.rs
@@ -13,21 +13,13 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    // Note: The actual address might be different for you. Use the address that
-    // your page fault handler reports.
-    let ptr = 0x20399c as *mut u32;
+    use x86_64::registers::control::Cr3;
 
-    // read from a code page
-    unsafe {
-        let x = *ptr;
-    }
-    println!("read worked");
-
-    // write to a code page
-    unsafe {
-        *ptr = 42;
-    }
-    println!("write worked");
+    let (level_4_page_table, _) = Cr3::read();
+    println!(
+        "Level 4 page table at: {:?}",
+        level_4_page_table.start_address()
+    );
 
     // as before
     #[cfg(test)]
