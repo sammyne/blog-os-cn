b7795b36e2282461f820761ed420e2ef3416810f
sammyne <lixiangmin01@baidu.com>
2020-07-17 20:43:30 +0800

    feature(09-paging-implementation): demo enabling 'map_physical_memory' for bootloader crate in 'Bootloader Support'

---
+++ 09-paging-implementation/README.md
@@ -0,0 +1,10 @@
+# 硬件中断
+
+## 运行
+
+```bash
+cargo xrun
+```
+
+## 参考文献
+- [Hardware Interrupts](https://os.phil-opp.com/hardware-interrupts/)
---
+++ 09-paging-implementation/src/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use blog_os::println;
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World!");
+
+    blog_os::init();
+
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!(
+        "Level 4 page table at: {:?}",
+        level_4_page_table.start_address()
+    );
+
+    // as before
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    blog_os::hlt_loop();
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    blog_os::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
