4b2ce1baab97f457c695b861a534f73955690734
sammyne <lixiangmin01@baidu.com>
2020-07-17 20:59:24 +0800

    feature(09-paging-implementation): demo 'Using OffsetPageTable'

--- 09-paging-implementation/src/main.rs
+++ 09-paging-implementation/src/main.rs
@@ -12,13 +12,16 @@ use bootloader::{entry_point, BootInfo};
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use blog_os::memory::translate_addr;
-    use x86_64::VirtAddr;
+    use x86_64::{structures::paging::MapperAllSizes, VirtAddr};
+
+    use blog_os::memory;
 
     println!("Hello World!");
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // new: initialize a mapper
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
     let addresses = [
         // the identity-mapped vga buffer page
@@ -33,7 +36,8 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        // new: use the `mapper.translate_addr` method
+        let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
 
