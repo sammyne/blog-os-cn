e1853b1fe49ab1f47804d17a3c17d508bc065917
sammyne <lixiangmin01@baidu.com>
2020-07-21 13:21:40 +0800

    feature(03-vga-text-mode): demo 'Text Buffer'

--- 03-vga-text-mode/src/vga_buffer.rs
+++ 03-vga-text-mode/src/vga_buffer.rs
@@ -29,3 +29,24 @@ impl ColorCode {
         ColorCode((background as u8) << 4 | (foreground as u8))
     }
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
