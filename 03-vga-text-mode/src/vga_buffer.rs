bdf53999181d51999200fe6f5b5b3b726a8f6a5f
sammyne <lixiangmin01@baidu.com>
2020-07-21 13:35:24 +0800

    feature(03-vga-text-mode): demo 'Spinlocks'

--- 03-vga-text-mode/src/vga_buffer.rs
+++ 03-vga-text-mode/src/vga_buffer.rs
@@ -1,14 +1,15 @@
 use core::fmt;
 
 use lazy_static::lazy_static;
+use spin::Mutex;
 use volatile::Volatile;
 
 lazy_static! {
-    pub static ref WRITER: Writer = Writer {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+    });
 }
 
 #[allow(dead_code)]
