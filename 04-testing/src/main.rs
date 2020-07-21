9834c7f7e627685b26f56565c372ecbd12dbe672
sammyne <lixiangmin01@baidu.com>
2020-07-21 18:40:56 +0800

    feature(04-testing): demo 'Using the Exit Device' and 'Success Exit Code'

--- 04-testing/src/main.rs
+++ 04-testing/src/main.rs
@@ -10,6 +10,22 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 /// This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -33,6 +49,8 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
