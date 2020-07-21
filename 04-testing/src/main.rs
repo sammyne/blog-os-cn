04dc3329f34f40925f4828fe5c5cdea66faacb35
sammyne <lixiangmin01@baidu.com>
2020-07-21 18:49:03 +0800

    feature(04-testing): demo 'Print an Error Message on Panic'

--- 04-testing/src/main.rs
+++ 04-testing/src/main.rs
@@ -27,10 +27,21 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
-/// This function is called on panic.
+// our existing panic handler
+#[cfg(not(test))] // new attribute
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
