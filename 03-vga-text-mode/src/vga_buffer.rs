68ca5d2700ac5e7a285b346f0ddb34e92372ebe4
sammyne <lixiangmin01@baidu.com>
2020-07-21 13:31:33 +0800

    feature(03-vga-text-mode): demo 'A Global Interface'

--- 03-vga-text-mode/src/vga_buffer.rs
+++ 03-vga-text-mode/src/vga_buffer.rs
@@ -2,6 +2,12 @@ use core::fmt;
 
 use volatile::Volatile;
 
+pub static WRITER: Writer = Writer {
+    column_position: 0,
+    color_code: ColorCode::new(Color::Yellow, Color::Black),
+    buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+};
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
