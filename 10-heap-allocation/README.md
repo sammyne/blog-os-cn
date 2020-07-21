a6857bc7c7b47e17caf3b0fff59cdbe3ae3de123
sammyne <lixiangmin01@baidu.com>
2020-07-21 09:51:45 +0800

    feature(10-heap-allocation): demo 'The Allocator Interface'

---
+++ 10-heap-allocation/README.md
@@ -0,0 +1,11 @@
+# 硬件中断
+
+## 运行
+
+```bash
+cargo xrun
+```
+
+## 参考文献
+- [Hardware Interrupts](https://os.phil-opp.com/hardware-interrupts/)
+- [rust-toolchain](https://github.com/rust-lang/rustup#the-toolchain-file)
---
+++ 10-heap-allocation/src/main.rs
@@ -0,0 +1,54 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use blog_os::println;
+use core::panic::PanicInfo;
+
+use bootloader::{entry_point, BootInfo};
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use x86_64::{structures::paging::Page, VirtAddr};
+
+    use blog_os::memory::{self, BootInfoFrameAllocator};
+
+    println!("Hello World!");
+    blog_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
+    // as before
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    blog_os::hlt_loop();
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    blog_os::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
