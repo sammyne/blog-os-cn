89bcf1296e41a224b1ac19331deb991b6fa790e5
sammyne <lixiangmin01@baidu.com>
2020-07-21 13:24:33 +0800

    feature(03-vga-text-mode): demo 'Try it out!'

--- 03-vga-text-mode/src/main.rs
+++ 03-vga-text-mode/src/main.rs
@@ -7,8 +7,6 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
-
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -17,14 +15,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
