396d5216b8e2103787fd2a65c69031a625a8e757
sammyne <lixiangmin01@baidu.com>
2020-07-23 13:50:05 +0800

    feature(05-cpu-exceptions): demo 'Lazy Statics to the Rescue'

--- 05-cpu-exceptions/src/interrupts.rs
+++ 05-cpu-exceptions/src/interrupts.rs
@@ -1,15 +1,18 @@
+use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 use crate::println;
 
-static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
 
 pub fn init_idt() {
-    unsafe {
-        IDT.breakpoint.set_handler_fn(breakpoint_handler);
-
-        IDT.load();
-    }
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
