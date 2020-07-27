f47781aa29efb3743a48df3af3377bc7571908e9
sammyne <lixiangmin01@baidu.com>
2020-07-27 18:52:36 +0800

    feature(11-allocator-designs): demo 'The alloc_from_region Function'

--- 11-allocator-designs/src/allocator/linked_list.rs
+++ 11-allocator-designs/src/allocator/linked_list.rs
@@ -24,6 +24,30 @@ impl LinkedListAllocator {
         self.add_free_region(heap_start, heap_size);
     }
 
+    /// Try to use the given region for an allocation with given size and
+    /// alignment.
+    ///
+    /// Returns the allocation start address on success.
+    fn alloc_from_region(region: &ListNode, size: usize, align: usize) -> Result<usize, ()> {
+        let alloc_start = align_up(region.start_addr(), align);
+        let alloc_end = alloc_start.checked_add(size).ok_or(())?;
+
+        if alloc_end > region.end_addr() {
+            // region too small
+            return Err(());
+        }
+
+        let excess_size = region.end_addr() - alloc_end;
+        if excess_size > 0 && excess_size < mem::size_of::<ListNode>() {
+            // rest of region too small to hold a ListNode (required because the
+            // allocation splits the region in a used and a free part)
+            return Err(());
+        }
+
+        // region suitable for allocation
+        Ok(alloc_start)
+    }
+
     /// Looks for a free region with the given size and alignment and removes
     /// it from the list.
     ///
