8aa6f8700ca62181acd040d783a1ab60f8327139
sammyne <lixiangmin01@baidu.com>
2020-07-27 13:23:26 +0800

    feature(11-async-await): demo 'Filling the Queue'

--- 11-async-await/blog-os/src/task/mod.rs
+++ 11-async-await/blog-os/src/task/mod.rs
@@ -2,8 +2,8 @@ use alloc::boxed::Box;
 use core::task::{Context, Poll};
 use core::{future::Future, pin::Pin};
 
-pub mod simple_executor;
 pub mod keyboard;
+pub mod simple_executor;
 
 pub struct Task {
     future: Pin<Box<dyn Future<Output = ()>>>,
