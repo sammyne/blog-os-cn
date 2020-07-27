1a9164b2789232951d537dfefd0ed9ae291520d0
sammyne <lixiangmin01@baidu.com>
2020-07-27 13:07:24 +0800

    feature(11-async-await): demo 'Simple Executor'

---
+++ 11-async-await/blog-os/src/task/simple_executor.rs
@@ -0,0 +1,19 @@
+use alloc::collections::VecDeque;
+
+use super::Task;
+
+pub struct SimpleExecutor {
+    task_queue: VecDeque<Task>,
+}
+
+impl SimpleExecutor {
+    pub fn new() -> SimpleExecutor {
+        SimpleExecutor {
+            task_queue: VecDeque::new(),
+        }
+    }
+
+    pub fn spawn(&mut self, task: Task) {
+        self.task_queue.push_back(task)
+    }
+}
