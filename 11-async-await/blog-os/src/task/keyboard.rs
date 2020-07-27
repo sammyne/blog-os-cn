5d42564bbc4d3a6e9669e4fd2b8507bcc7cf95ba
sammyne <lixiangmin01@baidu.com>
2020-07-27 13:29:11 +0800

    feature(11-async-await): demo 'Waking the Stored Waker'

--- 11-async-await/blog-os/src/task/keyboard.rs
+++ 11-async-await/blog-os/src/task/keyboard.rs
@@ -52,6 +52,8 @@ pub(crate) fn add_scancode(scancode: u8) {
     if let Ok(queue) = SCANCODE_QUEUE.try_get() {
         if let Err(_) = queue.push(scancode) {
             println!("WARNING: scancode queue full; dropping keyboard input");
+        } else {
+            WAKER.wake(); // new
         }
     } else {
         println!("WARNING: scancode queue uninitialized");
