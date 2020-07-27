1e3efea5d9115bc507f1c62cdc19e136c7b57290
sammyne <lixiangmin01@baidu.com>
2020-07-27 13:15:59 +0800

    feature(11-async-await): demo 'Trying It'

--- 11-async-await/blog-os/src/main.rs
+++ 11-async-await/blog-os/src/main.rs
@@ -6,11 +6,11 @@
 
 extern crate alloc;
 
-use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use core::panic::PanicInfo;
 
 use bootloader::{entry_point, BootInfo};
 
+use blog_os::task::{simple_executor::SimpleExecutor, Task};
 use blog_os::{allocator, println};
 
 entry_point!(kernel_main);
@@ -37,28 +37,9 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!(
-        "current reference count is {}",
-        Rc::strong_count(&cloned_reference)
-    );
-    core::mem::drop(reference_counted);
-    println!(
-        "reference count is {} now",
-        Rc::strong_count(&cloned_reference)
-    );
+    let mut executor = SimpleExecutor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.run();
 
     // as before
     #[cfg(test)]
