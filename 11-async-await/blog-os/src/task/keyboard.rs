93a6cc92b521da12b7e132c4db2699b8225fdfdd
sammyne <lixiangmin01@baidu.com>
2020-07-27 13:24:19 +0800

    feature(11-async-await): demo 'Scancode Stream'

--- 11-async-await/blog-os/src/task/keyboard.rs
+++ 11-async-await/blog-os/src/task/keyboard.rs
@@ -5,6 +5,19 @@ use crate::println;
 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
 
+pub struct ScancodeStream {
+    _private: (),
+}
+
+impl ScancodeStream {
+    pub fn new() -> Self {
+        SCANCODE_QUEUE
+            .try_init_once(|| ArrayQueue::new(100))
+            .expect("ScancodeStream::new should only be called once");
+        ScancodeStream { _private: () }
+    }
+}
+
 /// Called by the keyboard interrupt handler
 ///
 /// Must not block or allocate.
