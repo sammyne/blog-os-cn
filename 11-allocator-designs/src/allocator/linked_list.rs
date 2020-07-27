46668fb6350672e4fae356d1f6eee068c89e282e
sammyne <lixiangmin01@baidu.com>
2020-07-27 18:53:40 +0800

    feature(11-allocator-designs): demo 'Implementing GlobalAlloc'

--- 11-allocator-designs/src/allocator/linked_list.rs
+++ 11-allocator-designs/src/allocator/linked_list.rs
@@ -1,3 +1,8 @@
+use alloc::alloc::{GlobalAlloc, Layout};
+use core::ptr;
+
+use super::Locked;
+
 pub struct LinkedListAllocator {
     head: ListNode,
 }
@@ -99,5 +104,31 @@ impl ListNode {
 
     fn end_addr(&self) -> usize {
         self.start_addr() + self.size
+    }
+}
+
+unsafe impl GlobalAlloc for Locked<LinkedListAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        // perform layout adjustments
+        let (size, align) = LinkedListAllocator::size_align(layout);
+        let mut allocator = self.lock();
+
+        if let Some((region, alloc_start)) = allocator.find_region(size, align) {
+            let alloc_end = alloc_start.checked_add(size).expect("overflow");
+            let excess_size = region.end_addr() - alloc_end;
+            if excess_size > 0 {
+                allocator.add_free_region(alloc_end, excess_size);
+            }
+            alloc_start as *mut u8
+        } else {
+            ptr::null_mut()
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        // perform layout adjustments
+        let (size, _) = LinkedListAllocator::size_align(layout);
+
+        self.lock().add_free_region(ptr as usize, size)
     }
 }
